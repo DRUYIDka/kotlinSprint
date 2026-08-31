c10edd5db6cd4168a72284d8579091e3e075d62b
DRUYIDka <reptiliaanna@gmail.com>
2026-09-01 00:17:51 +0300

    KS-3-1: string greeting

---
+++ src/main/kotlin/KS-3-1.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var greeting = ""
+    val nameUser = "Anya"
+
+    greeting = "Good morning"
+    println("$greeting, $nameUser")
+    greeting = "Good evening"
+    println("$greeting, $nameUser")
+}

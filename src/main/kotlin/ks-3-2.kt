3db96799a2b4c3e047505c784849bd0d5ef13fda
DRUYIDka <reptiliaanna@gmail.com>
2026-09-01 19:00:39 +0300

    KS-3-2: replace surname

---
+++ src/main/kotlin/ks-3-2.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Татьяна"
+    val surname = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $patronymic, $age")
+    age = 22
+    println("$surname $name $patronymic, $age")
+}

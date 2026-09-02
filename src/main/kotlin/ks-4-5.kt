ed4a6af7f0079e448ffb9e0f7f8c54bb5b6a3d60
DRUYIDka <reptiliaanna@gmail.com>
2026-09-02 22:48:53 +0300

    KS-4-5: ship

--- src/main/kotlin/ks-4-5.kt
+++ src/main/kotlin/ks-4-5.kt
@@ -1,14 +1,14 @@
 fun main() {
     val isDamageShip = readln().toBoolean()
-    val crew : Int = readln().toInt()
+    val crew: Int = readln().toInt()
     val boxWithEat: Int = readln().toInt()
     val isSunnyDay = readln().toBoolean()
 
     val isNecessaryCrew = crew in 55..70
     val isManyEat = boxWithEat > 50
-    val compressionConditionFull = (isDamageShip == true && isNecessaryCrew && isManyEat) || (isDamageShip == false && boxWithEat >= 50 && isSunnyDay == true && crew == 70)
+    val compressionConditionFull = (!isDamageShip && isNecessaryCrew && isManyEat) || (boxWithEat >= 50 && isSunnyDay && crew == 70)
 
-    if(compressionConditionFull)
+    if (compressionConditionFull)
         println("Корабль готов отплыть")
     else
         println("Корабль не готов")

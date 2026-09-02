32c9253a71042d5db737a06cc4dff64008ffe298
DRUYIDka <reptiliaanna@gmail.com>
2026-09-02 15:26:50 +0300

    KS-4-4: new sport training

--- src/main/kotlin/ks-4-4.kt
+++ src/main/kotlin/ks-4-4.kt
@@ -4,19 +4,17 @@ fun main() {
     val isFootDay = false
     val isBackDay = false
     val isTorceDay = true
+    var finalString: String
 
-    if(dayTraining % 2 == 0)
-        println("""
-            |Упражнения для рук: ${!isHandsDay}
-            |Упражнения для ног: ${!isFootDay}
-            |Упражнения для спины: ${!isBackDay}
-            |Упражнения для пресса: ${!isTorceDay}
-        """.trimMargin())
+    if(dayTraining % 2 != 0)
+        finalString = "Упражнения для рук:    $isHandsDay\n" +
+                "Упражнения для ног:    $isFootDay\n" +
+                "Упражнения для спины:  ${(isBackDay)}\n" +
+                "Упражнения для пресса: $isTorceDay";
     else
-        println("""
-            |Упражнения для рук: ${isHandsDay}
-            |Упражнения для ног: ${isFootDay}
-            |Упражнения для спины: ${isBackDay}
-            |Упражнения для пресса: ${isTorceDay}
-        """.trimMargin())
+        finalString = "Упражнения для рук:    $isHandsDay\n" +
+                "Упражнения для ног:   $isFootDay\n" +
+                "Упражнения для спины: $isBackDay\n" +
+                "Упражнения для пресса: $isTorceDay";
+    println(finalString)
 }

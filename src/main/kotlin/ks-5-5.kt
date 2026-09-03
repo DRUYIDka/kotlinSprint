ce661b13e01d9ac565c7b9372e2be143371ad99a
DRUYIDka <reptiliaanna@gmail.com>
2026-09-03 21:27:43 +0300

    KS-5-5: loteria update

--- src/main/kotlin/ks-5-5.kt
+++ src/main/kotlin/ks-5-5.kt
@@ -1,10 +1,11 @@
 import kotlin.random.Random
-fun main() {
-    val randomNumber = List(3) {Random.nextInt(0, 42)}
-    val inputNumbers = List(3){readln().toInt()}
-    val completeResult = randomNumber intersect setOf(inputNumbers)
 
-    when (completeResult.size) {
+fun main() {
+    val randomNumber = List(3) { Random.nextInt(0, 43) }
+    val completeResult = randomNumber intersect setOf(List(3) { readln().toInt() })
+    val sizeResult = completeResult.size
+
+    when (sizeResult) {
         3 -> println("Вы угадали все числа, и выйграли джекпот!")
         2 -> println("Вы угадали два числа и получаете крупный приз!")
         1 -> println("Вы угадали одно число и получаете утешительный приз!")

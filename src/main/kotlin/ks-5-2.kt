9297d77cfd100f6c1e0b01a7ce3e8e8663b5d010
DRUYIDka <reptiliaanna@gmail.com>
2026-09-03 19:41:05 +0300

    KS-5-2: stop content

---
+++ src/main/kotlin/ks-5-2.kt
@@ -0,0 +1,12 @@
+const val AGE_OF_LEGAL = 18
+fun main() {
+    val inputYear = readln().toInt()
+
+    val result = if ((2026 - inputYear) >= AGE_OF_LEGAL) {
+        "Показать экран со скрытым контентом"
+    } else {
+        "Доступ запрещён"
+    }
+
+    println(result)
+}

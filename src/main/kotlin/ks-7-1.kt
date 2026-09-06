84d6275dc1d13830517166f3e23de7140714d0d4
DRUYIDka <reptiliaanna@gmail.com>
2026-09-06 11:35:14 +0300

    KS-7-1: password generated

--- src/main/kotlin/ks-7-1.kt
+++ src/main/kotlin/ks-7-1.kt
@@ -1,11 +1,13 @@
 import kotlin.random.Random
 
 fun main() {
-    val char = "abcdefghigklmnopqrstuvwxyz0123456789"
+    val char = "abcdefghigklmnopqrstuvwxyz"
+    val num = "0123456789"
     var password = ""
 
-    for(i in 1..6){
+    for(i in 1..6) {
         password += char[Random.nextInt(char.length)]
+        password += num[Random.nextInt(num.length)]
     }
     println(password)
 }

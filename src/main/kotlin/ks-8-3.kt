65a8f642703f178171592c597758ed2f352b4e19
DRUYIDka <reptiliaanna@gmail.com>
2026-09-07 20:52:34 +0300

    KS-8-3: list ingridients update

--- src/main/kotlin/ks-8-3.kt
+++ src/main/kotlin/ks-8-3.kt
@@ -1,10 +1,10 @@
 fun main() {
     val recipes = arrayOf("зелень", "петрушка", "колбаса", "сыр")
-    println("Введите ингредиент для поиска")
+    println("Какой ингредиент вы хотите найти?")
     val ingredient = readln()
 
     if (ingredient in recipes) {
-        println("Ингредиент $ingredient в рецепте есть")
+        println("Этот ингредиент в рецепте есть")
     } else
         println("Такого ингредиента в рецепте нет")
 

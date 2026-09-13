41234ae8397319c4beb20df16acc38b17fee7e36
DRUYIDka <reptiliaanna@gmail.com>
2026-09-13 22:26:23 +0300

    KS-10-2: length autentification

--- src/main/kotlin/ks-10-2.kt
+++ src/main/kotlin/ks-10-2.kt
@@ -1,12 +1,15 @@
 fun main() {
     println("Введите логин")
     val loginInput = readln()
+
     println("Введите пароль")
     val passwordInput = readln()
     val isValidFunction = isCheckedPassword(loginInput, passwordInput)
 
     if (!isValidFunction) println("Логин или пароль недостаточно длинные")
+    else println("Регистрация прошла успешно")
 }
-fun isCheckedPassword (login: String, password: String): Boolean {
-    return login.length < 4 && password.length < 4
+
+fun isCheckedPassword(login: String, password: String): Boolean {
+    return login.length < 4 || password.length < 4
 }

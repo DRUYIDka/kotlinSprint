08b978d7d33cc2ed300ec3d4ce5706c997ad1bac
DRUYIDka <reptiliaanna@gmail.com>
2026-09-21 21:22:08 +0300

    KS-10-5: avtorisation

--- src/main/kotlin/ks-10-5.kt
+++ src/main/kotlin/ks-10-5.kt
@@ -2,10 +2,10 @@ const val VALID_LOGIN = "user"
 const val VALID_PASSWORD = "123456"
 
 fun main() {
-    println("Введите логин")
-    val loginInput = readln()
-    println("Введите пароль")
-    val passwordInput = readln()
+    println("Логин")
+    val loginInput = VALID_LOGIN
+    println("Пароль")
+    val passwordInput = VALID_PASSWORD
     val fullLine = ('a'..'z') + ('0'..'9')
     val token: String?
     val isTrueCredentials = ((loginInput == VALID_LOGIN) && (passwordInput == VALID_PASSWORD))
@@ -15,13 +15,12 @@ fun main() {
 
     } else token = null
 
-    getBasket(token)
+    if (token != null) {
+        println("Корзина:\n ${getBasket(token).joinToString("\n")}")
+    } else println("Неверный логин или пароль!")
 }
 
-fun getBasket(token: String?) {
+fun getBasket(token: String?): List<String> {
     val listShopping = mutableListOf("футболка", "шорты", "худи")
-
-    if (token != null) {
-        println("Корзина:\n ${listShopping.joinToString("\n")}")
-    } else println("Неверный логин или пароль!")
+    return listShopping
 }

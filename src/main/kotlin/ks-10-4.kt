9ebc6891bf107d6f5896b90a85d555912b2d10e1
DRUYIDka <reptiliaanna@gmail.com>
2026-09-21 11:46:14 +0300

    KS-10-4: update play

--- src/main/kotlin/ks-10-4.kt
+++ src/main/kotlin/ks-10-4.kt
@@ -1,23 +1,30 @@
 fun main() {
-    var gainsPeople = 1
-    println("Кость бросает человек")
-    val resultPeople = generateValue()
-    println(resultPeople)
-    println("Кость бросает робот")
-    val resultRobot = generateValue()
-    println(resultRobot)
-    if (resultPeople > resultRobot)
-        gainsPeople++
-    println("Хотите бросить кости еще раз? Введите Да или Нет")
-    val answerPeople = readln()
-    repeatRound(answerPeople, gainsPeople)
+    var answerPeople = "Да"
+    while (answerPeople == "Да") {
+        repeatRound(answerPeople)
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        answerPeople = readln()
+        if (answerPeople == "Нет") {
+            repeatRound(answerPeople)
+            return
+        }
+    }
 }
-
 fun generateValue() = (1..6).random()
-fun repeatRound(answer: String, gains: Int){
-    if(answer == "Да") main()
-    else if(answer == "Нет"){
-        println("Выйгрышных партий человека - $gains")
+fun repeatRound(answer: String) {
+    var gainsPeople = 0
+    val resultPeople = generateValue()
+    val resultRobot = generateValue()
+    if (answer == "Да") {
+        println("Кость бросает человек")
+        println(resultPeople)
+        println("Кость бросает робот")
+        println(resultRobot)
+    }
+    if (resultPeople > resultRobot) gainsPeople++
+    if (answer == "Нет") {
+        println("Выйгрышных партий человека - $gainsPeople")
+        return
     }
 }
 

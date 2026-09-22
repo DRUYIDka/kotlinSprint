2e02c7ceea134435dd33b973bd49dd6b63aa6a22
DRUYIDka <reptiliaanna@gmail.com>
2026-09-22 10:31:26 +0300

    KS-10-5: avtorisation

--- src/main/kotlin/ks-10-5.kt
+++ src/main/kotlin/ks-10-5.kt
@@ -2,25 +2,27 @@ const val VALID_LOGIN = "user"
 const val VALID_PASSWORD = "123456"
 
 fun main() {
-    println("Логин")
     val loginInput = VALID_LOGIN
-    println("Пароль")
     val passwordInput = VALID_PASSWORD
-    val fullLine = ('a'..'z') + ('0'..'9')
-    val token: String?
-    val isTrueCredentials = ((loginInput == VALID_LOGIN) && (passwordInput == VALID_PASSWORD))
-
-    if (isTrueCredentials) {
-        token = (1..32).map { fullLine.random() }.joinToString("")
-
-    } else token = null
-
-    if (token != null) {
-        println("Корзина:\n ${getBasket(token).joinToString("\n")}")
+    if (getToken(loginInput, passwordInput) != null) {
+        println("Корзина:\n${getBasket(getToken(loginInput, passwordInput)).joinToString("\n")}")
     } else println("Неверный логин или пароль!")
 }
 
 fun getBasket(token: String?): List<String> {
-    val listShopping = mutableListOf("футболка", "шорты", "худи")
+    val listShopping = listOf("футболка", "шорты", "худи")
     return listShopping
+}
+
+fun getToken(login : String, password : String) : String? {
+
+    val fullLine = ('a'..'z') + ('0'..'9')
+    val token: String?
+    val isTrueCredentials = ((login == VALID_LOGIN) && (password == VALID_PASSWORD))
+
+    if (isTrueCredentials) {
+        token = (1..32).map { fullLine.random() }.joinToString("")
+    } else token = null
+
+    return token
 }
